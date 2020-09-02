1353e82981e8d8d0390040d9187afbd5f94d24c5
22zcorr <22zcorr@athenian.org>
2020-09-02 02:37:29 +0000

    I wrote the chatbot code :D

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
 
+import javax.sound.sampled.SourceDataLine;
+
 public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-
+        System.out.println("What is your name?");
+        String name = scan.next();
+        System.out.println("Hello, " + name + "! It's nice to meet you!");
+        System.out.println("What is your favorite color?");
+        String color = scan.next();
+        System.out.println("Oh I like " + color + " too!");
+       
         scan.close();
     }
 }
